9f7fafeaa846979da0feb0985cbc37902d616bff
Konrad Lazutka <konradlazutka@gmail.com>
2019-03-24 14:12:58 +0000

    Added remaining controllers and created views

--- RecruitmentPortal/RecruitmentPortal/Controllers/TermsConditionsController.cs
+++ RecruitmentPortal/RecruitmentPortal/Controllers/TermsConditionsController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace RecruitmentPortal.Controllers
 {
-    public class HomeController : Controller
+    public class TermsConditionsController : Controller
     {
         public ActionResult Detail()
         {
--- RecruitmentPortal/RecruitmentPortal/Controllers/VacanciesController.cs
+++ RecruitmentPortal/RecruitmentPortal/Controllers/VacanciesController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace RecruitmentPortal.Controllers
 {
-    public class HomeController : Controller
+    public class VacanciesController : Controller
     {
         public ActionResult Detail()
         {
